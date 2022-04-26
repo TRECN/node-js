9f8a7e7425da77da3d2c4de17df394ad0097d848
TRECN <rishabh1103@outlook.com>
2022-04-26 21:25:13 +0530

    commit

--- nodePractice/OSModules/OSModules.js
+++ nodePractice/OSModules/OSModules.js
@@ -6,3 +6,12 @@ console.log(user)
 
 // method returns the system uptime in seconds
 console.log(`the system Uptime is ${os.uptime()} seconds`)
+
+const currentOs={
+    name:os.type(),
+    release:os.release(),
+    totalMem:os.totalmem(),
+    freeMem: os.freemem(),
+}
+
+console.log(currentOs)
