db0032a884ed0c6ad100afe727cf64e128060da1
TRECN <rishabh1103@outlook.com>
2022-04-27 23:09:59 +0530

    commit

--- nodePractice/FileSystemModules/FS.js
+++ nodePractice/FileSystemModules/FS.js
@@ -1,10 +1,16 @@
 const {readFileSync,writeFileSync} = require('fs')
-
+//readFileSync takes two parameters 1: path, 2: type of file
 const first = readFileSync('./content/first.txt','utf8')
 const second = readFileSync('./content/second.txt','utf8')
 
 console.log(first,second)
 
-writeFileSync('./content/result.txt',`Here is the result: ${first}, ${second}`)
+//writeFileSync takes two parameters 1: path, 2: content to write
+writeFileSync(
+    './content/result.txt',
+    `Here is the result: ${first}, ${second}`,
+    {flag:'a'}
+    )
 
-cons
+
+
