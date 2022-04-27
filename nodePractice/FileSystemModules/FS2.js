8c4208634654bd0b73099f05d3850acf05a064b1
TRECN <rishabh1103@outlook.com>
2022-04-27 23:14:35 +0530

    commit

--- nodePractice/FileSystemModules/FS2.js
+++ nodePractice/FileSystemModules/FS2.js
@@ -1,2 +1,9 @@
-const {readFile,writeFile} = require('fs');
+const {readFile,writeFile, read} = require('fs');
 
+readFile('./content/first.txt','utf8',(err,result)=>{
+    if(err){
+        console.log(err)
+        return;
+    }
+    console.log(result)
+})
