fcc7cbbddd81aaeade1ad2d3c246d37d426b28d2
TRECN <rishabh1103@outlook.com>
2022-07-30 18:19:02 +0530

    copmmit

--- mernStack/routes/api/books.js
+++ mernStack/routes/api/books.js
@@ -14,13 +14,28 @@ router.get('/',(req,res)=>{
 
 router.get('/:id',(req,res)=>{
     Book.findById(req.params.id)
-    .then(book=>res.json(book))
-    .catch(er=>res.status(404).json({nobooksfund:'No Books Found'}));
+        .then(book=>res.json(book))
+        .catch(er=>res.status(404).json({nobooksfund:'No Books Found'}));
 
-})
+});
 
 router.post('/',(req,res)=>{
     Book.create(req.body)
-    .then(book=>res.json({msg:'Book added successfully'}));
-    .catch(er=>res.status(400).json({error:'Unable to add book'}))
-})
+        .then(book=>res.json({msg:'Book added successfully'}))
+        .catch(er => res.status(400).json({error:'Unable to add book'}));
+});
+
+router.put('/:id',(req,res)=>{
+    Book.findByIdAndUpdate(req.params.id,req.body)
+        .then(book=>res.json({msg:'Updated Successfully'}))
+        .catch(er => res.status(400).json({error:'Unable to update'}));
+});
+
+
+router.delete('/:id',(req,res)=>{
+    Book.findByIdAndRemove(req.params.id,req.body)
+        .then(book=>res.json({msg:'Book deleted successfully'}))
+        .catch(er => res.status(404).json({error:'NO such book'}));
+});
+
+module.exports=router;
