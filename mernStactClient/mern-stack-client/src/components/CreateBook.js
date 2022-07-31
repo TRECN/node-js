0f3b2f54d1e6da5af0b2a41ed45b25d3f19066c0
TRECN <rishabh1103@outlook.com>
2022-07-31 18:31:18 +0530

    commit

--- mernStactClient/mern-stack-client/src/components/CreateBook.js
+++ mernStactClient/mern-stack-client/src/components/CreateBook.js
@@ -1,6 +1,37 @@
 import React, { Component } from 'react'
 
 export default class CreateBook extends Component {
+
+    constructor(){
+        super();
+        this.state={
+            title: '',
+            isbn:'',
+            author:'',
+            description:'',
+            published_date:'',
+            publisher:''
+        };
+
+        onChange =e=>{
+            this.state({[e.target.name]:e.target.value})
+        }
+
+        onSubmit=e=>{
+            e.preventDefault();
+
+            const date={
+                title: this.state.title,
+                isbn: this.state.isbn,
+                author: this.state.author,
+                description: this.state.description,
+                published_date
+
+            }
+        }
+    }
+
+
   render() {
     return (
       <div>CreateBook</div>
