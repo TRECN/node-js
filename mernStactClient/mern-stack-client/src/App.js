f0f39c04f9a54f9109ef85c8e05aa6e5830737a3
TRECN <rishabh1103@outlook.com>
2022-07-31 18:20:33 +0530

    commit

--- mernStactClient/mern-stack-client/src/App.js
+++ mernStactClient/mern-stack-client/src/App.js
@@ -10,9 +10,14 @@ import UpdateBookInfo from './components/UpdateBookInfo'
 class App extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <Router>
+        <div>
+          <Route exact path='/' component={ShowBookList} />
+          <Route path='/create-book' component={CreateBook} />
+          <Route path='/edit-book/:id' component={UpdateBookInfo} />
+          <Route path='/show-book/:id' component={ShowBookDetails} />
+        </div>
+      </Router>
     );
   }
 }
