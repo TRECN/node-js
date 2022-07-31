d4ff598ea047c2d66ebdd9ca29c91fe00ce91b50
TRECN <rishabh1103@outlook.com>
2022-07-31 18:16:44 +0530

    commit

--- mernStactClient/mern-stack-client/src/App.js
+++ mernStactClient/mern-stack-client/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router,Route } from 'react-router-dom';
 
-import CreateBook from '../components/CreateBook'
-import BookCard from '../components/BookCard'
-import ShowBookDetails from '../components/ShowBookDetails'
-import ShowBookList from '../components/ShowBookList'
-import UpdateBookInfo from '../components/UpdateBookInfo'
+import CreateBook from './components/CreateBook'
+import BookCard from './components/BookCard'
+import ShowBookDetails from './components/ShowBookDetails'
+import ShowBookList from './components/ShowBookList'
+import UpdateBookInfo from './components/UpdateBookInfo'
 
 class App extends Component {
   render() {
