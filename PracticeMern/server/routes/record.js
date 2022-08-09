10abbc56a9d0fc09e9dab2a05ccd915bfe2e094b
TRECN <rishabh1103@outlook.com>
2022-08-09 21:02:44 +0530

    commit

--- PracticeMern/server/routes/record.js
+++ PracticeMern/server/routes/record.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express')
 
 const recordRoutes = express.Router();
@@ -5,6 +6,74 @@ const recordRoutes = express.Router();
 const dbo=require('../db/base');
 
 const ObjectId = require('mongodb').ObjectId;
+
+recordRoutes.route('/record').get((req,res)=>{
+    let db_connect = dbo.getDb('employees');
+    db_connect
+        .collection('records')
+        .find({})
+        .toArray((er,result)=>{
+            if(er)throw er;
+            res.json(result);
+        });
+});
+
+recordRoutes.route('/record/:id').get((req,res)=>{
+    let db_connect=dbo.getDb();
+    let myquery = {_id: ObjectId(req.params.id)};
+    db_connect
+        .collection('records')
+        .findOne(myquery,(er,result)=>{
+            if(er)throw er;
+            res.json(result);
+        });
+});
+
+recordRoutes.route('/record/add').post((req,res)=>{
+    let db_connect=dbo.getDb();
+    let myobj={
+        name:req.body.name,
+        position:req.body.position,
+        level:req.body.level,
+    };
+    db_connect
+        .collection('records')
+        .insertOne(myobj,(er,result)=>{
+            if(er)throw er;
+            res.json(result);
+        });
+});
+
+recordRoutes.route('/update/:id').post((req,res)=>{
+    let db_connect=dbo.getDb();
+    let myquery={_id:ObjectId(req.params.id)};
+    let newval={
+        $set:{
+            name:req.body.name,
+            position:req.body.position,
+            level:req.body.level,
+        },
+    };
+    db_connect
+        .collection('records')
+        .updateOne(myquery,newval,(er,result)=>{
+            if(er)throw er;
+            crossOriginIsolated.log('1 document updated');
+            res.json(result);
+        });
+});
+
+recordRoutes.route('/:id').delete((req,res)=>{
+    let db_connect=dbo.getDb();
+    let myquery={_id:ObjectId(req.params.id)};
+    db_connect
+        .collection('records')
+        .deleteOne(myquery,(er,result)=>{
+            if(er)throw er;
+            console.log('1 document deleted');
+            res.json(result)
+        });
+});
 
 
     
