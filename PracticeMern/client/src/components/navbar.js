a4b946ed24e3a700e2dede10e32b4ea8b3b5c7c1
TRECN <rishabh1103@outlook.com>
2022-08-11 18:17:42 +0530

    commit

--- PracticeMern/client/src/components/navbar.js
+++ PracticeMern/client/src/components/navbar.js
@@ -22,6 +22,16 @@ export default function navbar() {
             >
                 <span className='navbar-toggler-icon'></span>
             </button>
+
+            <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+              <ul className='navbar-nav ml-auto'>
+                <li className='nav-item'>
+                    <NavLink className='nav-link' to='/create'>
+                      create Record
+                    </NavLink>
+                </li>
+              </ul>
+            </div>
         </nav>
 
     </div>
