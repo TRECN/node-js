4bde079f71c337a3859a585d6c7b2fa445ca2ffd
TRECN <rishabh1103@outlook.com>
2022-08-15 06:45:06 +0530

    commit

--- PracticeMern/client/src/components/recordList.js
+++ PracticeMern/client/src/components/recordList.js
@@ -73,7 +73,7 @@ export default function RecordList() {
           </tr>
         </thead>
         <tbody>
-          {recordList()}
+          {recordList}
         </tbody>
       </table>
     </div>
