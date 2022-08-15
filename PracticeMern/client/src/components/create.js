7b8d1b736c989739efa94d386bd67036e64b21b7
TRECN <rishabh1103@outlook.com>
2022-08-15 05:46:37 +0530

    commit

--- PracticeMern/client/src/components/create.js
+++ PracticeMern/client/src/components/create.js
@@ -19,7 +19,25 @@ export default function create() {
   const onSubmit =async(e)=>{
     e.preventDefault();
 
-    
+    const newPerson={...form};
+
+    await fetch("http://localhost:5000/record/add",{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json",
+
+      },
+      body:JSON.stringify(newPerson),
+    })
+    .catch(err=>{
+      window.alert(err);
+      return;
+    });
+    setForm({name:'',
+    position:'',
+    level:'',});
+
+    navigate('/')
   }
 
   return (
