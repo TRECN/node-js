104364a66e539de173a17c9632912501c09850de
TRECN <rishabh1103@outlook.com>
2022-08-15 05:39:09 +0530

    commit

--- PracticeMern/client/src/components/create.js
+++ PracticeMern/client/src/components/create.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 export default function create() {
+
+  const [form,setForm]=useState({
+    name:'',
+    position:'',
+    level:'',
+  });
+  const navigate = useNavigate();
+
   return (
     <div>create</div>
   )
