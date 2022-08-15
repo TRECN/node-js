b559de79a05b0af5d502f0d97e3dc16e2120d603
TRECN <rishabh1103@outlook.com>
2022-08-15 05:36:13 +0530

    commit

--- PracticeMern/client/src/App.js
+++ PracticeMern/client/src/App.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { Routes } from 'react-router-dom';
+import Navbar from './components/navbar';
+const App=()=>{
+    return(
+        <div>
+            <Navbar/>
+            <Routes>
+                
+            </Routes>
+        </div>
 
-export default function App() {
-  return (
-    <div>App</div>
-  )
-}
+    );
+};
+
+export default App;
