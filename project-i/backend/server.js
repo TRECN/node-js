43e33982d81a050d58a4431bcf1620f9da57b273
TRECN <rishabh1103@outlook.com>
2022-08-16 07:55:19 +0530

    commit

--- project-i/backend/server.js
+++ project-i/backend/server.js
@@ -5,3 +5,20 @@ const path = require('path');
 const app = express();
 require('./database')
 
+app.use(bodyParser.json());
+app.use(cors());
+
+const users = require('/api/users');
+app.use('/api/users',users);
+
+app.use(express.static(path.join(_dirname,'../build')))
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(_dirname,'../build'))
+
+});
+
+const port=process.env.PORT || 5000;
+
+app.listen(port,()=>{
+    console.log(`Server started on port ${port}`)
+})
