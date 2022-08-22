52cc65dc92be7fe517c29bb6639f4cdd5d1b9d0d
TRECN <rishabh1103@outlook.com>
2022-08-22 22:19:19 +0530

    commit

---
+++ project-i/backend/db/main.js
@@ -0,0 +1,19 @@
+const {MongoClient} = require('mongodb');
+const Db=process.env.DB_URI;
+const client =  new MongoClient(Db,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+});
+
+var _db;
+
+module.exports={
+    connectToServer:(callback)=>{
+        client.connect((er,db)=>{
+            if(db){
+                _db=db.db('passcodes');
+                console.log('Successfully connected to mongoDb')
+            }
+        })
+    }
+}
