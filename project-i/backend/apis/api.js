12eae504e2df6289a12e74a0202e464b27df5d70
TRECN <rishabh1103@outlook.com>
2022-08-23 17:54:15 +0530

    commit

--- project-i/backend/apis/api.js
+++ project-i/backend/apis/api.js
@@ -17,5 +17,15 @@ apiRoutes.route('/api').get((req,res)=>{
             res.json(result)
         })
 })
+apiRoutes.route('/api/:id').get((req,res)=>{
+    const db_connect=dbo.getDb();
+    const myquery={_id:ObjectId(req.params.id)}
+    db_connect
+        .collection('apis')
+        .findOne(myquery,(er,result)=>{
+            if(er)throw er;
+            res.json(result);
+        })
+})
 
 module.exports=apiRoutes;
