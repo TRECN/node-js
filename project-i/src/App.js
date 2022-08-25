aea7c2eed0c4894f7c1e7873b016ff470175f399
TRECN <rishabh1103@outlook.com>
2022-08-25 21:35:01 +0530

    commit

--- project-i/src/App.js
+++ project-i/src/App.js
@@ -1,14 +1,3 @@
-import React from 'react'
-import {Routes,Route} from 'react-router-dom'
-import CreatePass from './components/createpass'
-function App() {
-  return (
-    <div>
-      <Routes>
-        <Route exact path='/' element={<CreatePass/>} />
-      </Routes>
-    </div>
-  )
-}
+
 
 export default App
