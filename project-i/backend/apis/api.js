94568316d438e031707e810888cda0fb516dcb60
TRECN <rishabh1103@outlook.com>
2022-08-26 22:09:16 +0530

    commit

--- project-i/backend/apis/api.js
+++ project-i/backend/apis/api.js
@@ -7,7 +7,16 @@ const dbo=require('../db/main')
 
 const ObjectId = require('mongodb').ObjectId;
 
-
+apiRoutes.route('/api').get((req,res)=>{
+    let db_connect=dbo.getDb('passcodes');
+    db_connect
+        .collection('apis')
+        .find({})
+        .toArray((er,result)=>{
+            if(er) throw er;
+            res.json(result)
+        })
+})
 apiRoutes.route('/api/:id').get((req,res)=>{
     let db_connect=dbo.getDb();
     let myquery={_id:ObjectId(req.params.id)}
