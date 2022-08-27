9662db043dbbaf701b3cdfdefc37acd8c41267d4
TRECN <rishabh1103@outlook.com>
2022-08-27 06:37:13 +0530

    commit

--- project-i/src/components/createpass.js
+++ project-i/src/components/createpass.js
@@ -7,6 +7,11 @@ function CreatePass() {
       <div className="innerForm">
         <form action="">
           <h1 className='head'>Generate Password</h1>
+          <div className="enter">
+            <label htmlFor="">title</label>
+            <input type="text" value="" placeholder='enter the title'/>
+            
+          </div>
         </form>
       </div>
     </div>
