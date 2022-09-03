b51b4036725e4ada91846daa6914e66d0f67a4b1
TRECN <rishabh1103@outlook.com>
2022-09-03 21:46:33 +0530

    commit

--- project-i/src/App.js
+++ project-i/src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import {Routes,Route} from 'react-router-dom'
 import CreatePass from './components/createpass'
 import NavBar from './components/NavBar'
+import Saved from './components/Saved'
 function App() {
   return (
     <div>
       <NavBar/>
       <Routes>
         <Route exact path='/' element={<CreatePass/>} />
+        <Route path='/saved' element={<Saved/>} />
       </Routes>
     </div>
   )
