386c9377adc6808aeedd5389e8776b4abe8d4699
TRECN <rishabh1103@outlook.com>
2022-09-04 17:20:07 +0530

    commit

--- project-i/src/components/Saved.js
+++ project-i/src/components/Saved.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styling/saved.css'
 
 
 
-const content=(props)=>(
+const Content=(props)=>(
   <tr>
     <td>{props.record.title}</td>
     <td>{props.record.pass}</td>
@@ -11,10 +11,51 @@ const content=(props)=>(
 )
 
 function Saved() {
+
+  const [records,setRecord]=useState([]);
+
+  useEffect(()=>{
+    const getRecords=async()=>{
+      const response = await fetch(`http://localhost:5000/api/`);
+
+      if(!response.ok){
+        window.alert(`An err has occurred : ${response.statusText}`);
+        return;
+      }
+      const records=await response.json();
+      setRecord(records);
+      console.log(records);
+    }
+    getRecords()
+    return;
+  },[records.length])
+
+  const list=()=>{
+    return records.map((record)=>{
+      return(
+        <Content
+          record={record}
+          key={record._id}
+        />
+      )
+    })
+  }
+
   return (
     <div className="container">
       <div className="innerForm">
-
+        <h3>Saved Passcode</h3>
+        <table className="table table-striped" style={{marginTop:20}}>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>PassCode</th>
+          </tr>
+        </thead>
+        <tbody>
+          {list()}
+        </tbody>
+      </table>
       </div>
     </div>
   )
